b167601a1c54f83d4433946c0d793ababf98969c
0918nobita <nobita.0918@gmail.com>
2021-05-18 15:45:13 +0900

    :sparkles: 式木から関数を生成するサンプルを追加した

--- Program.fs
+++ Program.fs
@@ -1,8 +1,18 @@
 module Program
 
+// open Microsoft.FSharp.Reflection
+open System
+open System.Linq.Expressions
+open System.Reflection
 open System.Reflection.Emit
 
-type Delegate1 = delegate of int * int -> int
+type User = User of id : int * name : string
+
+type CSharpFunc =
+    static member ToFSharpFunc<'a, 'b>(func : Func<'a, 'b>) : 'a -> 'b =
+        fun a -> func.Invoke(a)
+    static member ToFSharpFunc<'a, 'b, 'c>(func : Func<'a, 'b, 'c>) : 'a -> 'b -> 'c =
+        fun a b -> func.Invoke(a, b)
 
 [<EntryPoint>]
 let main _ =
@@ -12,15 +22,40 @@ let main _ =
                     returnType = intType,
                     parameterTypes = [| intType; intType |])
 
-    let ilGenerator = method.GetILGenerator()
-    ilGenerator.Emit OpCodes.Ldarg_0
-    ilGenerator.Emit OpCodes.Ldarg_1
-    ilGenerator.Emit OpCodes.Add
-    ilGenerator.Emit OpCodes.Ret
+    let gen = method.GetILGenerator()
+    gen.Emit OpCodes.Ldarg_0
+    gen.Emit OpCodes.Ldarg_1
+    gen.Emit OpCodes.Add
+    gen.Emit OpCodes.Ret
 
-    let addDelegate : Delegate1 =
-        downcast method.CreateDelegate(delegateType = typeof<Delegate1>)
+    let addIL =
+        method.CreateDelegate(typeof<Func<int, int, int>>) :?> Func<int, int, int>
+        |> CSharpFunc.ToFSharpFunc
 
-    addDelegate.Invoke(1, 2)
-    |> printfn "%d" // => 3
+    let paramX = Expression.Parameter(intType, "x")
+    let paramY = Expression.Parameter(intType, "y")
+    let lambda = Expression.Lambda(Expression.Add(paramX, paramY), [paramX; paramY])
+    let addE =
+        lambda.Compile() :?> Func<int, int, int>
+        |> CSharpFunc.ToFSharpFunc
+
+    printfn "%d" <| addIL 1 2 // => 3
+    printfn "%d" <| addE 1 2  // => 3
+
+    let methodInfo : MethodInfo = downcast typeof<User>.GetMember("NewUser").[0]
+    let paramName = Expression.Parameter(typeof<string>, "name")
+    let expr =
+        Expression.Lambda(
+            Expression.Call(
+                methodInfo,
+                [|
+                    Expression.Constant 21 :> Expression
+                    paramName :> Expression
+                |]),
+            [| paramName |])
+    let createUser =
+        expr.Compile() :?> Func<string, User>
+        |> CSharpFunc.ToFSharpFunc
+
+    printfn "%A" <| createUser "Alice"
     0
