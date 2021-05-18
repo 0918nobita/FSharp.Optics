591ff55aa041a5d98c5e2e5a18ecdcb97c96ad62
0918nobita <nobita.0918@gmail.com>
2021-05-18 17:10:21 +0900

    :sparkles: DLL を生成するサンプルを追加した

--- Program.fs
+++ Program.fs
@@ -1,61 +1,32 @@
 module Program
 
-// open Microsoft.FSharp.Reflection
-open System
-open System.Linq.Expressions
 open System.Reflection
 open System.Reflection.Emit
 
-type User = User of id : int * name : string
-
-type CSharpFunc =
-    static member ToFSharpFunc<'a, 'b>(func : Func<'a, 'b>) : 'a -> 'b =
-        fun a -> func.Invoke(a)
-    static member ToFSharpFunc<'a, 'b, 'c>(func : Func<'a, 'b, 'c>) : 'a -> 'b -> 'c =
-        fun a b -> func.Invoke(a, b)
-
 [<EntryPoint>]
 let main _ =
+    let asmName = AssemblyName("DynAsm")
+
+    let asmBuilder =
+        AssemblyBuilder.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndCollect)
+    let modBuilder = asmBuilder.DefineDynamicModule(asmName.Name)
+    let typeBuilder = modBuilder.DefineType("MyDynamicType", TypeAttributes.Public)
     let intType = typeof<int>
-    let method = DynamicMethod(
-                    name = "add",
-                    returnType = intType,
-                    parameterTypes = [| intType; intType |])
+    let methodBuilder =
+        typeBuilder.DefineMethod(
+            "addFunc",
+            MethodAttributes.Public ||| MethodAttributes.Static,
+            returnType = intType,
+            parameterTypes = [| intType; intType |])
 
-    let gen = method.GetILGenerator()
-    gen.Emit OpCodes.Ldarg_0
-    gen.Emit OpCodes.Ldarg_1
-    gen.Emit OpCodes.Add
-    gen.Emit OpCodes.Ret
+    let ilGen = methodBuilder.GetILGenerator()
+    ilGen.Emit OpCodes.Ldarg_0
+    ilGen.Emit OpCodes.Ldarg_1
+    ilGen.Emit OpCodes.Add
+    ilGen.Emit OpCodes.Ret
 
-    let addIL =
-        method.CreateDelegate(typeof<Func<int, int, int>>) :?> Func<int, int, int>
-        |> CSharpFunc.ToFSharpFunc
+    ignore <| typeBuilder.CreateType()
 
-    let paramX = Expression.Parameter(intType, "x")
-    let paramY = Expression.Parameter(intType, "y")
-    let lambda = Expression.Lambda(Expression.Add(paramX, paramY), [paramX; paramY])
-    let addE =
-        lambda.Compile() :?> Func<int, int, int>
-        |> CSharpFunc.ToFSharpFunc
-
-    printfn "%d" <| addIL 1 2 // => 3
-    printfn "%d" <| addE 1 2  // => 3
-
-    let methodInfo : MethodInfo = downcast typeof<User>.GetMember("NewUser").[0]
-    let paramName = Expression.Parameter(typeof<string>, "name")
-    let expr =
-        Expression.Lambda(
-            Expression.Call(
-                methodInfo,
-                [|
-                    Expression.Constant 21 :> Expression
-                    paramName :> Expression
-                |]),
-            [| paramName |])
-    let createUser =
-        expr.Compile() :?> Func<string, User>
-        |> CSharpFunc.ToFSharpFunc
-
-    printfn "%A" <| createUser "Alice"
+    let asmGen = Lokad.ILPack.AssemblyGenerator()
+    asmGen.GenerateAssembly(asmBuilder, asmName.Name + ".dll")
     0
