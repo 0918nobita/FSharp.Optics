26d5ed933bd979579997cb82944d378cb9496952
0918nobita <nobita.0918@gmail.com>
2021-05-18 12:41:37 +0900

    :sparkles: Reflection.Emit を用いてデリゲートを生成するサンプルを追加した

--- Program.fs
+++ Program.fs
@@ -1,5 +1,26 @@
 module Program
 
+open System.Reflection.Emit
+
+type Delegate1 = delegate of int * int -> int
+
 [<EntryPoint>]
-let main argv =
+let main _ =
+    let intType = typeof<int>
+    let method = DynamicMethod(
+                    name = "add",
+                    returnType = intType,
+                    parameterTypes = [| intType; intType |])
+
+    let ilGenerator = method.GetILGenerator()
+    ilGenerator.Emit OpCodes.Ldarg_0
+    ilGenerator.Emit OpCodes.Ldarg_1
+    ilGenerator.Emit OpCodes.Add
+    ilGenerator.Emit OpCodes.Ret
+
+    let addDelegate : Delegate1 =
+        downcast method.CreateDelegate(delegateType = typeof<Delegate1>)
+
+    addDelegate.Invoke(1, 2)
+    |> printfn "%d" // => 3
     0
