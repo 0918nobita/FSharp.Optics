988efe383fa1914b5c1b190308b43e93ae842580
0918nobita <nobita.0918@gmail.com>
2021-05-18 21:21:34 +0900

    :sparkles: F# Compiler Service を用いたサンプルを追加した

--- Program.fs
+++ Program.fs
@@ -1,32 +1,73 @@
 module Program
 
-open System.Reflection
-open System.Reflection.Emit
+open System
+open System.IO
+open FSharp.Compiler.SourceCodeServices
+
+type FsProject = FsProject of fsproj : string * src : string
+
+let genFsProject () : FsProject =
+    let base1 = Path.GetTempFileName()
+    let srcFileName = Path.ChangeExtension(base1, ".fs")
+    let srcFileContent = """module Program
+
+type User = User of id : int * name : string
+
+let add x y = x + y
+"""
+    File.WriteAllText(srcFileName, srcFileContent)
+    let base2 = Path.GetTempFileName()
+    let projFileName = Path.ChangeExtension(base2, ".fsproj")
+    FsProject (projFileName, srcFileName)
 
 [<EntryPoint>]
 let main _ =
-    let asmName = AssemblyName("DynAsm")
+    let (FsProject (fsproj, src)) = genFsProject ()
 
-    let asmBuilder =
-        AssemblyBuilder.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.RunAndCollect)
-    let modBuilder = asmBuilder.DefineDynamicModule(asmName.Name)
-    let typeBuilder = modBuilder.DefineType("MyDynamicType", TypeAttributes.Public)
-    let intType = typeof<int>
-    let methodBuilder =
-        typeBuilder.DefineMethod(
-            "addFunc",
-            MethodAttributes.Public ||| MethodAttributes.Static,
-            returnType = intType,
-            parameterTypes = [| intType; intType |])
+    let checker = FSharpChecker.Create()
 
-    let ilGen = methodBuilder.GetILGenerator()
-    ilGen.Emit OpCodes.Ldarg_0
-    ilGen.Emit OpCodes.Ldarg_1
-    ilGen.Emit OpCodes.Add
-    ilGen.Emit OpCodes.Ret
+    let projOptions =
+        let runtimeDir = Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory()
+        let baseDir = AppContext.BaseDirectory
+        checker.GetProjectOptionsFromCommandLineArgs(
+            fsproj,
+            [|
+                "--noframework"
+                "--target:library"
+                src
+                "-r:" + Path.Combine(runtimeDir, "mscorlib.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Collections.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Core.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Net.Requests.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Net.WebClient.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Private.CoreLib.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Runtime.dll")
+                "-r:" + Path.Combine(runtimeDir, "System.Runtime.Numerics.dll")
+                "-r:" + Path.Combine(baseDir, "FSharp.Core.dll")
+            |])
 
-    ignore <| typeBuilder.CreateType()
+    let wholeProjResults =
+        checker.ParseAndCheckProject(projOptions)
+        |> Async.RunSynchronously
 
-    let asmGen = Lokad.ILPack.AssemblyGenerator()
-    asmGen.GenerateAssembly(asmBuilder, asmName.Name + ".dll")
+    let errors = wholeProjResults.Errors
+    if not (Array.isEmpty errors)
+        then
+            eprintfn "%A" <| errors
+            exit 1
+
+    wholeProjResults.AssemblySignature.Entities
+    |> Seq.iter (fun x ->
+        printfn "DisplayName: %s" x.DisplayName
+
+        printfn "NestedEntries:"
+        x.NestedEntities
+        |> Seq.iter (fun y ->
+            printfn "    %A" y)
+
+        printfn "MembersFunctionsAndValues:"
+        x.MembersFunctionsAndValues
+        |> Seq.iter (fun y ->
+            printfn "    %A" y))
     0
